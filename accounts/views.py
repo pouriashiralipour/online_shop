a3b6127a7a0c89bf65e60678b90dc23bc30cf28a
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-05 08:34:56 +0430

    Create signup page

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm
 
 
-class SignUPView(generic.CreateView):
+class SignUpView(generic.CreateView):
     template_name = 'registration/signup.html'
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
