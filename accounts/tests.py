ed7fecb5dc28c2c3860c0f48cd3685e78f6ac0d3
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-05 08:56:27 +0430

    Add tests to accounts app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class AccountsPageTest(TestCase):
+
+    def test_login_page_url(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_url_by_name(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_template_used(self):
+        response = self.client.get(reverse('login'))
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_signup_page_template_used(self):
+        response = self.client.get(reverse('signup'))
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_login_page_content(self):
+        response = self.client.get(reverse('login'))
+        self.assertContains(response, 'LogIn')
+
+    def test_signup_page_content(self):
+        response = self.client.get(reverse('signup'))
+        self.assertContains(response, 'Sign UP')
