2590c6afe4e373cdcb22f7e51cdf455504de3c3f
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-05 16:08:04 +0430

    allauth config

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-from django.views import generic
-from django.urls import reverse_lazy
-
-from .forms import CustomUserCreationForm
-
-
-class SignUpView(generic.CreateView):
-    template_name = 'registration/signup.html'
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+# from django.shortcuts import render
+# from django.views import generic
+# from django.urls import reverse_lazy
+#
+# from .forms import CustomUserCreationForm
+#
+#
+# class SignUpView(generic.CreateView):
+#     template_name = 'registration/signup.html'
+#     form_class = CustomUserCreationForm
+#     success_url = reverse_lazy('login')
