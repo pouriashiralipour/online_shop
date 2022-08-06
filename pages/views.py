a7a42a438351634083c6236f586f85df76d4e574
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-06 18:04:07 +0430

    Create home page and listview same together

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 
+from products import models
 
-class HomePageView(generic.TemplateView):
-    template_name = 'home.html'
+
+class HomePageView(generic.ListView):
+    template_name = 'products/product_list_view.html'
+    # model = Product
+    queryset = models.Product.objects.filter(active=True)
+    context_object_name = 'products'
 
 
 class AboutUsView(generic.TemplateView):
