cdbcbf3b07ac8eee2a8616c52c239b4cfe826c08
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-06 11:18:06 +0430

    Change display on admin panel for products

--- products/admin.py
+++ products/admin.py
@@ -5,4 +5,8 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description', 'price', 'datetime_created', 'status', 'active', ]
+    list_display = ['title', 'price', 'status', 'active', ]
+
+    def __str__(self):
+        return f'{self.title}'
+
