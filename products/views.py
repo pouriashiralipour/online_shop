67099d00f176af6327d2be3dc669f19c16d9a784
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-07 08:47:15 +0430

    Create commnetcreateview

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Product
+from .models import Product, Comments
+from .forms import CommentsForm
 
 
 class ProductListView(generic.ListView):
@@ -15,3 +16,18 @@ class ProductDetailsView(generic.DeleteView):
     template_name = 'products/product_details_view.html'
     model = Product
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_from'] = CommentsForm()
+        return context
+
+
+class CommentCreateView(generic.CreateView):
+    model = Comments
+    form = CommentsForm
+
+    def form_valid(self, form):
+        new_comment = form.save(commit=False)
+        new_comment.author = self.request.user
+        return super().form_valid(form)
