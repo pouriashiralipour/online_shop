e703a9239929e8f9da96ce9c8fa7dd0628ace02a
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-07 15:29:44 +0430

    rosetta add url and config

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,8 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
+
+    # rosetta app
+    path('rosetta/', include('rosetta.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
