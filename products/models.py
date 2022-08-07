bbd20a77dce2af358c71bb26f1f86fd3e3fbfc40
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-07 07:14:20 +0430

    Add Comments model to db

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Product(models.Model):
@@ -16,5 +17,26 @@ class Product(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
     def get_absolute_url(self):
         return reverse('product_details_view', args=[self.id])
+
+
+class Comments(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'Very Bad'),
+        ('2', 'Bad'),
+        ('3', 'Normal'),
+        ('4', 'Good'),
+        ('5', 'Perfect'),
+    ]
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, blank=True, null=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comment')
+    is_active = models.BooleanField()
+    recommend = models.BooleanField()
