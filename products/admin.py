544baedad2b79c4392180d6e420e2aa5fcdae24d
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-07 11:06:29 +0430

    Add CommentInline in admin panel

--- products/admin.py
+++ products/admin.py
@@ -3,12 +3,20 @@ from django.contrib import admin
 from .models import Product, Comments
 
 
+class CommentInline(admin.StackedInline):
+    model = Comments
+    fields = ['author', 'text', 'stars', 'is_active', 'recommend']
+    extra = 0
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'status', 'active', ]
+    inlines = [
+        CommentInline,
+    ]
 
 
 @admin.register(Comments)
-class ProductAdmin(admin.ModelAdmin):
+class CommentAdmin(admin.ModelAdmin):
     list_display = ['product', 'author', 'text', 'stars', 'is_active', 'recommend']
 
