429853855ef2737067437565dad13767e4497d73
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-21 08:07:41 +0430

    collect static files

--- config/settings.py
+++ config/settings.py
@@ -169,6 +169,7 @@ USE_L10N = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
