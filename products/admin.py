9f9bfa40abf165050e898e8b5ba9fbe948fc6416
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-22 09:00:36 +0430

    Add actions to admin

--- products/admin.py
+++ products/admin.py
@@ -6,11 +6,22 @@ from django.utils.translation import ngettext
 from .models import Product, Comments, Category
 
 
+@admin.action(description=_('Selected products are not available'))
 def make_published(modeladmin, request, queryset):
-    updated = queryset.update(status='ava')
+    updated = queryset.update(status='nav')
     modeladmin.message_user(request, ngettext(
         _('%d story was successfully marked as published.'),
         _('%d stories were successfully marked as published.'),
+        updated,
+    ) % updated, messages.SUCCESS)
+
+
+@admin.action(description=_('Selected products are not active'))
+def make_de_active(modeladmin, request, queryset):
+    updated = queryset.update(active=False)
+    modeladmin.message_user(request, ngettext(
+        _('%d story was successfully marked as deactivated.'),
+        _('%d stories were successfully marked as deactivated.'),
         updated,
     ) % updated, messages.SUCCESS)
 
@@ -27,7 +38,7 @@ class ProductAdmin(admin.ModelAdmin):
     ordering = ('datetime_created',)
     search_fields = ('title',)
     prepopulated_fields = {'slug': ('title',)}
-    actions = [make_published]
+    actions = [make_published, make_de_active]
     list_per_page = 25
     inlines = [
         CommentInline,
@@ -45,9 +56,29 @@ class CommentAdmin(admin.ModelAdmin):
     ordering = ('datetime_created',)
     list_per_page = 25
     search_fields = ('author',)
+    actions = ['make_active']
+
+    @admin.action(description=_('Selected products are active'))
+    def make_active(self, request, queryset):
+        updated = queryset.update(active=False)
+        self.message_user(request, ngettext(
+            _('%d story was successfully marked as activated.'),
+            _('%d stories were successfully marked as activated.'),
+            updated,
+        ) % updated, messages.SUCCESS)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status', 'parent']
     prepopulated_fields = {'slug': ('title',)}
+    actions = ['make_de_active']
+
+    @admin.action(description=_('Selected products are de_active'))
+    def make_de_active(self, request, queryset):
+        updated = queryset.update(status=False)
+        self.message_user(request, ngettext(
+            _('%d story was successfully marked as deactivated.'),
+            _('%d stories were successfully marked as deactivated.'),
+            updated,
+        ) % updated, messages.SUCCESS)
