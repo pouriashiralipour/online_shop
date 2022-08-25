52b900d5c90c9dc9a6c066779f45751070f35bad
pouriashiralipour <pouriashiralipour@gmail.com>
2022-08-25 17:02:30 +0430

    add + and - quantity

--- cart/forms.py
+++ cart/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.utils.translation import gettext, gettext_lazy as _
 
 
 class AddToCartProductForm(forms.Form):
-    QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 30)]
+    QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 31)]
 
-    quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int)
+    quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int, label=_('quantity'))
