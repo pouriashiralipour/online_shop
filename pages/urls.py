b21c03badcc11d0ee4f81cb13858bf387ebcc2ab
Pouria <pouriashiralipour@gmail.com>
2023-08-03 01:18:14 +0330

    add urls pages to urls config

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from .views import HomePageView, AboutUsView
 
+
+app_name = 'pages'
 urlpatterns = [
-    path('', HomePageView.as_view(), 'home'),
-    path('about_us/', AboutUsView.as_view(), 'about_us'),
+    path('', HomePageView.as_view(), name='home'),
+    path('about_us/', AboutUsView.as_view(), name='about_us'),
 ]
