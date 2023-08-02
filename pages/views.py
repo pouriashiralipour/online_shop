97c5be75ca42b24037e1b955492ffac3ba097197
Pouria <pouriashiralipour@gmail.com>
2023-08-03 01:08:13 +0330

    create view for home and aboutus page

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class HomePageView(generic.TemplateView):
+    template_name = 'home.html'
+
+
+class AboutUsView(generic.TemplateView):
+    template_name = 'pages/about_us.html'
