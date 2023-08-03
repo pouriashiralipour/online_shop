79ac4a7e91771ba77085b1a91fc0b4eec11077df
Pouria <pouriashiralipour@gmail.com>
2023-08-03 22:18:53 +0330

    delete accounts url for django

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
 ]
