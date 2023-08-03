8918ea7e9d3e507f36dcb5d6d2e84c18529e9b4a
Pouria <pouriashiralipour@gmail.com>
2023-08-03 19:25:46 +0330

    install crispy forms package and add config to settings.py

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-met%!pjg1@hvc(w0207bm4!j!p!4mx=4*s*zg&0=giqstd70+g
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -41,6 +39,10 @@ INSTALLED_APPS = [
     # my apps
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
+
+    # third party
+    'crispy_forms',
+    'crispy_bootstrap4',
 
 ]
 
@@ -74,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -90,8 +91,6 @@ DATABASES = {
         # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -111,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -122,7 +120,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -139,3 +136,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # redirect
 LOGOUT_REDIRECT_URL = 'pages:home'
 LOGIN_REDIRECT_URL = 'pages:home'
+
+# crispy forms config
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
