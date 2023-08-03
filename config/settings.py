84b10494461bef8e291639b463bdc5a6933baee0
Pouria <pouriashiralipour@gmail.com>
2023-08-03 19:19:06 +0330

    add redirect config to settings

--- config/settings.py
+++ config/settings.py
@@ -137,5 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # redirect
-LOGOUT_REDIRECT_URL = 'home'
-LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'pages:home'
+LOGIN_REDIRECT_URL = 'pages:home'
