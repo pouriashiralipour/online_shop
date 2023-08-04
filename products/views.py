0152a1c7b06e48aa256d01ef46265b4d1b9eca69
Pouria <pouriashiralipour@gmail.com>
2023-08-04 14:43:44 +0330

    create template for detailview and create detailview

--- products/views.py
+++ products/views.py
@@ -8,3 +8,8 @@ class ProductsListView(generic.ListView):
     template_name = 'products/products_list_view.html'
     model = Products
     context_object_name = 'products'
+
+
+class ProductDetailsView(generic.DetailView):
+    model = Products
+    template_name = 'products/product_details_view.html'
