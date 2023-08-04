4424c97849b25608a24e772bef5ad4c288b878d4
Pouria <pouriashiralipour@gmail.com>
2023-08-04 14:16:29 +0330

    add products model fields

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext, gettext_lazy as _
 
-# Create your models here.
+
+class Products(models.Model):
+    STATUS_CHOICES = (
+        ('ava', _('available')),
+        ('not', _('not_available')),
+    )
+    title = models.CharField(max_length=200, verbose_name=_('title'))
+    slug = models.SlugField(max_length=500, unique=True, allow_unicode=True, verbose_name=_('slug'))
+    description = models.TextField(verbose_name=_('description'))
+    price = models.PositiveIntegerField(default=0, verbose_name=_('price'))
+    active = models.BooleanField(default=True, verbose_name=_('active'))
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3, default='ava', verbose_name=_('status'))
+    datetime_created = models.DateTimeField(default=timezone.now, verbose_name=_('datetime_created'))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('datetime_modified'))
