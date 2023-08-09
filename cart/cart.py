c50f00058b489a56ca9bb5c7327698f0bfbaa4b3
Pouria <pouriashiralipour@gmail.com>
2023-08-09 18:33:32 +0330

    fix cart add problem

--- cart/cart.py
+++ cart/cart.py
@@ -1,10 +1,14 @@
-from products.models import Products
+from django.utils.translation import gettext as _
 from django.contrib import messages
-from django.utils.translation import gettext, gettext_lazy as _
+
+from products.models import Products
 
 
 class Cart:
     def __init__(self, request):
+        """
+        Initialize the cart
+        """
         self.request = request
 
         self.session = request.session
@@ -17,33 +21,42 @@ class Cart:
         self.cart = cart
 
     def add(self, product, quantity=1, replace_current_quantity=False):
+        """
+        Add the specified product to the cart if it exists
+        """
         product_id = str(product.id)
 
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': quantity}
+            self.cart[product_id] = {'quantity': 0}
+
         if replace_current_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
 
-        messages.success(self.request, _('Product successfully added to cart.'))
+        messages.success(self.request, _('Product successfully added to cart'))
 
         self.save()
 
     def remove(self, product):
+        """
+        Remove a product from the cart
+        """
         product_id = str(product.id)
 
         if product_id in self.cart:
             del self.cart[product_id]
-            messages.error(self.request, _('Product successfully removed from cart.'))
+            messages.success(self.request, _('Product successfully removed from cart'))
             self.save()
 
     def save(self):
+        """
+        Mark session as modified to save changes
+        """
         self.session.modified = True
 
     def __iter__(self):
         product_ids = self.cart.keys()
-
         products = Products.objects.filter(id__in=product_ids)
 
         cart = self.cart.copy()
@@ -66,3 +79,8 @@ class Cart:
         product_ids = self.cart.keys()
 
         return sum(item['quantity'] * item['product_obj'].price for item in self.cart.values())
+
+    def is_empty(self):
+        if self.cart:
+            return False
+        return True
