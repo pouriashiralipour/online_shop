23761282af2db961cdecdfff967793e2094c75d9
Pouria <pouriashiralipour@gmail.com>
2023-08-09 21:54:06 +0330

    adadd orderitem to views and save it to DB

--- orders/views.py
+++ orders/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from .forms import OrderForm
+from cart.cart import Cart
+from .models import OrderItem
 
 
 @login_required
 def order_create_view(request):
     order_form = OrderForm()
+    cart = Cart(request)
 
     if request.method == 'POST':
         order_form = OrderForm(request.POST)
@@ -14,6 +17,16 @@ def order_create_view(request):
             order_obj = order_form.save(commit=False)
             order_obj.user = request.user
             order_obj.save()
+
+            for item in cart:
+                product = item.product_obj
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=product,
+                    quantity=item['quantity'],
+                    price=product.price,
+                )
+            cart.clear()
 
     context = {'form': order_form}
     return render(request, 'orders/order_create.html', context)
