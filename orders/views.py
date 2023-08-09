2b3221700955c56b299573d770be244f93695de2
Pouria <pouriashiralipour@gmail.com>
2023-08-09 21:49:14 +0330

    add بخ forms to order_create and send to DB

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .forms import OrderForm
 
 
+@login_required
 def order_create_view(request):
-    context = {'form': OrderForm()}
+    order_form = OrderForm()
+
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            order_obj = order_form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+
+    context = {'form': order_form}
     return render(request, 'orders/order_create.html', context)
